781f37fc226133933a1da755651c73ffe2d235b6
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-07-06 12:40:22 -0400

    set custom lsp icons

--- nebvim/lua/neb/plugins/lsp/lsp-zero.lua
+++ nebvim/lua/neb/plugins/lsp/lsp-zero.lua
@@ -1,13 +1,30 @@
 require("lsp-zero")
 
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-require("lspconfig").bashls.setup({})
-require("lspconfig").lua_ls.setup({})
-require("lspconfig").nixd.setup({})
-require("lspconfig").html.setup({})
-require("lspconfig").jqls.setup({})
-require("lspconfig").jsonls.setup({})
-require("lspconfig").markdown_oxide.setup({}) -- obsidian markdown
--- require("lspconfig").marksman.setup({}) -- markdown
--- require("lspconfig").prosemd_lsp.setup({}) # markdown
--- require("lspconfig").remark_ls.setup({}) # markdown
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+lsp_zero.set_sign_icons({
+  error = '✘',
+  warn = '▲',
+  hint = '⚑',
+  info = '»'
+})
+
+-- Custom colors for diagnostic signs
+-- vim.cmd([[
+--   highlight DiagnosticSignError guifg=#ff0000 guibg=NONE
+--   highlight DiagnosticSignWarn guifg=#ffff00 guibg=NONE
+--   highlight DiagnosticSignHint guifg=#00ffff guibg=NONE
+--   highlight DiagnosticSignInfo guifg=#0000ff guibg=NONE
+-- ]])
+
+-- Optional: Change colors for the diagnostic text as well
+-- vim.cmd([[
+--   highlight DiagnosticError guifg=#ff0000
+--   highlight DiagnosticWarn guifg=#ffff00
+--   highlight DiagnosticHint guifg=#00ffff
+--   highlight DiagnosticInfo guifg=#0000ff
+-- ]])
