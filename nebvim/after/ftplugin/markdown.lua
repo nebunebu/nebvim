d0e91588cbee951d13b0601823a7d2a9f890fb9d
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-07-06 21:25:21 -0400

    add after/ftplugin/markdown.lua

---
+++ nebvim/after/ftplugin/markdown.lua
@@ -0,0 +1,10 @@
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.smarttab = true
+
+vim.opt_local.textwidth = 80  -- Wrap at 80 characters
+vim.opt_local.spell = true    -- Enable spell checking for Markdown files
+
+-- Prevent these settings from being overridden
+vim.b.did_ftplugin = 1
