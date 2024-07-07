09b04427503e70df7f799af13944b374ecf43f0a
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-07-07 01:36:58 -0400

    add note

--- nebvim/lua/neb/plugins/cmp.lua
+++ nebvim/lua/neb/plugins/cmp.lua
@@ -1,3 +1,4 @@
+-- BUG: supertab interfering with normal tab
 local cmp = require("cmp")
 -- local cmp_format = require("lsp-zero").cmp_format({details = true})
 local cmp_action = require("lsp-zero").cmp_action()
