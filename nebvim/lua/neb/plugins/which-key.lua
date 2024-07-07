f57b825c4afe66fae7f68a1be99b429f38e73c16
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-07-07 10:35:30 -0400

    update which-key

--- nebvim/lua/neb/plugins/which-key.lua
+++ nebvim/lua/neb/plugins/which-key.lua
@@ -3,66 +3,77 @@ require("which-key").setup({})
 local wk = require("which-key")
 
 wk.register({
-  ["<leader>"] = {
-    ["/"] = {
-      "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>",
-      "Toggle Line Comment",
-    },
-    h = {
-      "<cmd>nohlsearch<CR>",
-      "Clear Search Highlights",
-    },
-    t = {
-      "<cmd>Triptych<CR>",
-      "Open Triptych",
-    },
-    ["f"] = {
-      name = "Telescope",
-      f = {
-        "<cmd>Telescope find_files<CR>",
-        "Find files",
-      },
-      o = {
-        "<cmd>Telescope oldfiles<CR>",
-        "Find old files",
-      },
-      m = {
-        "<cmd>Telescope man_pages<CR>",
-        "Find man pages",
-      },
-      c = {
-        "<cmd>Telescope current_buffer_fuzzy_find<CR>",
-        "Find in current buffer",
-      },
-      h = {
-        "<cmd>Telescope help_tags<CR>",
-        "Find help tags",
-      },
-      p = {
-        "<cmd>Telescope projects<CR>",
-        "Find in projects",
-      },
-      g = {
-        "<cmd>Telescope live_grep<CR>",
-        "Find with live_grep",
-      },
-    },
-  },
+	["<leader>"] = {
+		["/"] = {
+			"<cmd>lua require('Comment.api').toggle.linewise.current()<CR>",
+			"Toggle Line Comment",
+		},
+		h = {
+			"<cmd>nohlsearch<CR>",
+			"Clear Search Highlights",
+		},
+		t = {
+			"<cmd>Triptych<CR>",
+			"Open Triptych",
+		},
+		["m"] = {
+			name = "Markdown",
+			v = {
+				"<cmd>Markview toggleAll<CR>",
+				"Toggle Markview",
+			},
+			c = {
+				"<cmd>GenerateTOC<CR>",
+				"Create Table of Contents",
+			},
+		},
+		["f"] = {
+			name = "Telescope",
+			f = {
+				"<cmd>Telescope find_files<CR>",
+				"Find files",
+			},
+			o = {
+				"<cmd>Telescope oldfiles<CR>",
+				"Find old files",
+			},
+			m = {
+				"<cmd>Telescope man_pages<CR>",
+				"Find man pages",
+			},
+			c = {
+				"<cmd>Telescope current_buffer_fuzzy_find<CR>",
+				"Find in current buffer",
+			},
+			h = {
+				"<cmd>Telescope help_tags<CR>",
+				"Find help tags",
+			},
+			p = {
+				"<cmd>Telescope projects<CR>",
+				"Find in projects",
+			},
+			g = {
+				"<cmd>Telescope live_grep<CR>",
+				"Find with live_grep",
+			},
+		},
+	},
 }, { mode = "n" })
 
 wk.register({
-  ["<"] = {
-    "<gv",
-    "Dedent and keep selection",
-  },
-  [">"] = {
-    ">gv",
-    "Dedent and keep selection",
-  },
-  ["<leader>"] = {
-    ["/"] = {
-      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-      "Toggle Line Comment",
-    },
-  },
+	["<"] = {
+		"<gv",
+		"Dedent and keep selection",
+	},
+	[">"] = {
+		">gv",
+		"Dedent and keep selection",
+	},
+	["<leader>"] = {
+		["/"] = {
+			"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+			"Toggle Line Comment",
+		},
+	},
 }, { mode = "x" })
