5f462dc2f3130cb90ab1d603ce12c29cd1f40630
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-07-15 21:55:31 -0400

    remove hidden cursor in alpha

--- nebvim/lua/neb/plugins/alpha.lua
+++ nebvim/lua/neb/plugins/alpha.lua
@@ -74,24 +74,26 @@ theta.config = {
 
 require("alpha").setup(theta.config)
 
-vim.api.nvim_create_autocmd("User", {
-	pattern = "AlphaReady",
-	desc = "hide cursor for alpha",
-	callback = function()
-		-- FIX: vim.api.nvim_get_hl_by_name deprecated
-		local hl = vim.api.nvim_get_hl_by_name("Cursor", true)
-		hl.blend = 100
-		vim.api.nvim_set_hl(0, "Cursor", hl)
-		vim.opt.guicursor:append("a:Cursor/lCursor")
-	end,
-})
-vim.api.nvim_create_autocmd("BufUnload", {
-	buffer = 0,
-	desc = "show cursor after alpha",
-	callback = function()
-		local hl = vim.api.nvim_get_hl_by_name("Cursor", true)
-		hl.blend = 0
-		vim.api.nvim_set_hl(0, "Cursor", hl)
-		vim.opt.guicursor:remove("a:Cursor/lCursor")
-	end,
-})
+-- NOTE: supposed to hide cursor
+-- BUG: cursor hidden when not in alpha
+-- vim.api.nvim_create_autocmd("User", {
+-- 	pattern = "AlphaReady",
+-- 	desc = "hide cursor for alpha",
+-- 	callback = function()
+-- 		-- FIX: vim.api.nvim_get_hl_by_name deprecated
+-- 		local hl = vim.api.nvim_get_hl_by_name("Cursor", true)
+-- 		hl.blend = 100
+-- 		vim.api.nvim_set_hl(0, "Cursor", hl)
+-- 		vim.opt.guicursor:append("a:Cursor/lCursor")
+-- 	end,
+-- })
+-- vim.api.nvim_create_autocmd("BufUnload", {
+-- 	buffer = 0,
+-- 	desc = "show cursor after alpha",
+-- 	callback = function()
+-- 		local hl = vim.api.nvim_get_hl_by_name("Cursor", true)
+-- 		hl.blend = 0
+-- 		vim.api.nvim_set_hl(0, "Cursor", hl)
+-- 		vim.opt.guicursor:remove("a:Cursor/lCursor")
+-- 	end,
+-- })
