97ccd6790e7ad70aeb6415bf8f015e02120840f3
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-07-18 19:57:02 -0400

    activate alpha

--- nebvim/init.lua
+++ nebvim/init.lua
@@ -2,7 +2,7 @@ require("neb.options")
 require("neb.colorscheme")
 -- require("neb.plugins.mkdnflow")
 require("neb.plugins.fortune") -- must be before alpha
--- require("neb.plugins.alpha")
+require("neb.plugins.alpha")
 require("neb.plugins.autopairs")
 require("neb.plugins.barbecue")
 require("neb.plugins.conform")
