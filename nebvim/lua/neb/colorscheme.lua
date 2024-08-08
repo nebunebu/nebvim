26bcb0520f17e13d4080b023e732f845932bebfd
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-08-07 20:09:43 -0400

    set up flow-nvim (not using)

--- nebvim/lua/neb/colorscheme.lua
+++ nebvim/lua/neb/colorscheme.lua
@@ -1,1 +1,10 @@
+-- require("flow").setup({
+-- 	transparent = false, -- Set transparent background.
+-- 	fluo_color = "pink", --  Fluo color: pink, yellow, orange, or green.
+-- 	mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+-- 	aggressive_spell = false, -- Display colors for spell check.
+-- })
+--
+-- vim.cmd("colorscheme flow")
+
 vim.cmd.colorscheme("rose-pine")
