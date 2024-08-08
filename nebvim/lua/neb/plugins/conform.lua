669afa4edf58d7ff201b99743739a78103d0d61b
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-08-07 20:32:47 -0400

    fix (maybe) formatters

--- nebvim/lua/neb/plugins/conform.lua
+++ nebvim/lua/neb/plugins/conform.lua
@@ -2,14 +2,7 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		markdown = { "markdownlint" },
-		-- nix = { "nixfmt" },
-    nix = function(bufnr)
-      if require("conform").get_formatter_info("nixpkgs_fmt", bufnr).available then
-        return { "nixpkgs_fmt" }
-      else
-        return { "nixfmt" }
-      end
-    end,
+		nix = { "nixpkgs_fmt", "nixfmt", stop_after_first = true },
 		javascript = { "prettierd" },
 	},
 })
