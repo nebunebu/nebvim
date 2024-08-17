7a9f440c0421584ece7bceb07cf03c0048a17254
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-08-16 23:39:40 -0400

    refactor vimPlugins into seperate file

---
+++ plugins.nix
@@ -0,0 +1,68 @@
+{ pkgs, ... }:
+{
+  inherit (pkgs.vimPlugins)
+    image-nvim
+    hardtime-nvim
+    precognition-nvim
+    nvim-surround
+    toggleterm-nvim
+    nix-develop-nvim
+    triptych-nvim
+    # markview-nvim
+    oil-nvim
+
+    nvim-colorizer-lua
+
+    rose-pine# colorscheme
+    which-key-nvim
+    plenary-nvim
+    nvim-web-devicons
+    nvim-navic
+    barbecue-nvim
+    lualine-nvim
+    alpha-nvim
+
+    marks-nvim
+    neogit
+    lsp-zero-nvim
+    nvim-lspconfig
+    lspkind-nvim
+    fidget-nvim
+
+    # cmp
+    luasnip
+    nvim-cmp
+    cmp-nvim-lsp
+    cmp-buffer
+    cmp_luasnip
+    cmp-async-path
+
+    # formatters
+    conform-nvim
+
+    # linters
+    nvim-lint
+
+    # editing support
+    indent-blankline-nvim
+    nvim-autopairs
+    rainbow-delimiters-nvim
+    nvim-treesitter-endwise
+
+    comment-nvim
+    todo-comments-nvim
+
+    # Telescope
+    telescope-nvim
+    telescope-project-nvim
+    # telescope-manix
+    quarto-nvim
+    otter-nvim
+    dressing-nvim
+
+    mkdnflow-nvim
+
+    # tmux
+    vim-tmux-navigator
+    ;
+}
