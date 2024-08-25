c2e38d5044c6d8830b8e0ccfd2a12e9c0cfeb349
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-08-25 15:40:28 -0400

    update

--- nebvim/lua/neb/plugins/lsp/lspconfig.lua
+++ nebvim/lua/neb/plugins/lsp/lspconfig.lua
@@ -1,19 +1,12 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
--- pkgs.nodePackages.bash-language-server system-wide
 require("lspconfig").bashls.setup({})
--- pkgs.nodePackages.jq-lsp -- NOTE: system-wide
-require("lspconfig").jqls.setup({})
--- NOTE: system-wide
+require("lspconfig").cssls.setup({}) -- css, scss, less
+require("lspconfig").eslint.setup({}) -- js
 require("lspconfig").jsonls.setup({})
 require("lspconfig").lua_ls.setup({})
--- obsidian markdown -- NOTE: system-wide
 require("lspconfig").markdown_oxide.setup({})
--- NOTE: system-wide
 require("lspconfig").nixd.setup({})
--- NOTE: system-wide
-require("lspconfig").yamlls.setup({})
 require("lspconfig").texlab.setup({}) -- latex
--- require("lspconfig").awk_ls.setup({})
-require("lspconfig").eslint.setup({}) -- js, ts
-require("lspconfig").cssls.setup({}) -- css, scss, less
+require("lspconfig").texlab.setup({}) -- latex
+require("lspconfig").lemminx.setup({})
