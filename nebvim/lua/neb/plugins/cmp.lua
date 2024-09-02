7c624a6fd0369f19ebab66e36aee5a1fba39d71c
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-09-02 16:54:45 -0400

    refactor

--- nebvim/lua/neb/plugins/cmp.lua
+++ nebvim/lua/neb/plugins/cmp.lua
@@ -1,28 +1,32 @@
 local cmp = require("cmp")
 local cmp_action = require("lsp-zero").cmp_action()
 
+local sources = {
+	{ name = "luasnip" },
+	{ name = "nvim_lsp" },
+	{ name = "buffer" },
+	{ name = "async_path" },
+	-- { name = "cmdline" },
+	-- { name = "cmd-cmdline-history" },
+	-- { name = "obsidian.nvim" },
+	-- { name = "dap" },
+	-- { name = "vim-dadbod-completion" },
+	-- { name = "cmp_yanky" },
+	-- { name = "dotenv" },
+}
+
+local formatting = {
+	fields = { "abbr", "kind", "menu" },
+	format = require("lspkind").cmp_format({
+		mode = "symbol_text", -- show only symbol annotations
+		maxwidth = 50, -- prevent the popup from showing more than provided characters
+		ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+	}),
+}
+
 cmp.setup({
-	sources = {
-		{ name = "luasnip" },
-		{ name = "nvim_lsp" },
-		{ name = "buffer" },
-		{ name = "async_path" },
-		-- { name = "cmdline" },
-		-- { name = "cmd-cmdline-history" },
-		-- { name = "obsidian.nvim" },
-		-- { name = "dap" },
-		-- { name = "vim-dadbod-completion" },
-		-- { name = "cmp_yanky" },
-		-- { name = "dotenv" },
-	},
-	formatting = {
-		fields = { "abbr", "kind", "menu" },
-		format = require("lspkind").cmp_format({
-			mode = "symbol_text", -- show only symbol annotations
-			maxwidth = 50, -- prevent the popup from showing more than provided characters
-			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-		}),
-	},
+	sources = sources,
+	formatting = formatting,
 	preselect = "item",
 	completion = {
 		completeopt = "menu,menuone,noinsert",
