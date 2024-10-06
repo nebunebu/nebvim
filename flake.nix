6edb8371f1509eeea0665c2cc497f172749d7d06
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-10-06 10:58:21 -0400

    move most plugins to ./plugins.nix

--- flake.nix
+++ flake.nix
@@ -36,7 +36,8 @@
               };
               config = {
                 plugins = mkFlakePlugins
-                  ++ [ pkgs.vimPlugins.nvim-treesitter.withAllGrammars ];
+                  ++ [ pkgs.vimPlugins.nvim-treesitter.withAllGrammars ]
+                  ++ builtins.attrValues (import ./plugins.nix { inherit pkgs; });
               };
             }).overrideAttrs
               (old: {
@@ -84,121 +85,16 @@
 
     # {{{  NOTE: Flake Inputs for plugins
 
-    obsidian-nvim = { url = "github:epwalsh/obsidian.nvim"; flake = false; };
-
-    # Utilities
-    plenary-nvim = { url = "github:nvim-lua/plenary.nvim"; flake = false; };
-
-    # LSP
-    lsp-zero-nvim = { url = "github:VonHeikemen/lsp-zero.nvim"; flake = false; };
-    lspconfig-nvim = { url = "github:neovim/nvim-lspconfig"; flake = false; };
-    lspkind-nvim = { url = "github:onsails/lspkind.nvim"; flake = false; };
-    fidget-nvim = { url = "github:j-hui/fidget.nvim"; flake = false; };
-    barbecue-nvim = { url = "github:utilyre/barbecue.nvim"; flake = false; };
-    nvim-navic-nvim = { url = "github:SmiteshP/nvim-navic"; flake = false; };
-
-    # DAP
-    dap-nvim = { url = "github:mfussenegger/nvim-dap"; flake = false; };
-    dap-ui-nvim = { url = "github:rcarriga/nvim-dap-ui"; flake = false; };
-    nio-nvim = { url = "github:nvim-neotest/nvim-nio"; flake = false; };
-    one-small-step-for-vimkind-nvim = { url = "github:jbyuki/one-small-step-for-vimkind"; flake = false; };
-    telescope-dap-nvim = { url = "github:nvim-telescope/telescope-dap.nvim"; flake = false; };
-
-    # Linting
-    nvim-lint-nvim = { url = "github:mfussenegger/nvim-lint"; flake = false; };
-    tiny-inline-diagnostic-nvim = { url = "github:rachartier/tiny-inline-diagnostic.nvim"; flake = false; };
-
-    # Formatters
-    conform-nvim = { url = "github:stevearc/conform.nvim"; flake = false; };
-
-    # Completion
-    nvim-cmp-nvim = { url = "github:hrsh7th/nvim-cmp"; flake = false; };
-    cmp-nvim-lsp-nvim = { url = "github:hrsh7th/cmp-nvim-lsp"; flake = false; };
-    cmp-buffer-nvim = { url = "github:hrsh7th/cmp-buffer"; flake = false; };
-    cmp-luasnip-nvim = { url = "github:saadparwaiz1/cmp_luasnip"; flake = false; };
-    cmp-async-path-nvim = { url = "github:FelipeLema/cmp-async-path"; flake = false; };
-
-    # Snippets
-    luasnip-nvim = { url = "github:L3MON4D3/LuaSnip"; flake = false; };
-
-    # Editing Support
-    indent-blankline-nvim = { url = "github:lukas-reineke/indent-blankline.nvim"; flake = false; };
-    nvim-autopairs-nvim = { url = "github:windwp/nvim-autopairs"; flake = false; };
-    nvim-treesitter-endwise-nvim = { url = "github:RRethy/nvim-treesitter-endwise"; flake = false; };
-    rainbow-delimitters-nvim = { url = "github:HiPhish/rainbow-delimiters.nvim"; flake = false; };
-    nvim-surround-nvim = { url = "github:kylechui/nvim-surround"; flake = false; };
-
-    # Comments
-    comment-nvim = { url = "github:numToStr/Comment.nvim"; flake = false; };
-    todo-comments-nvim = { url = "github:folke/todo-comments.nvim"; flake = false; };
-
-    # File Explorers
-    triptych-nvim = { url = "github:simonmclean/triptych.nvim"; flake = false; }; # Swap to yazi plugin
-    oil-nvim = { url = "github:stevearc/oil.nvim"; flake = false; };
-
-    # Telescope
-    telescope-nvim = { url = "github:nvim-telescope/telescope.nvim"; flake = false; };
-    telescope-project-nvim = { url = "github:nvim-telescope/telescope-project.nvim"; flake = false; }; # replace with whatever auto adds git repos
-    nerdy-nvim = { url = "github:2KAbhishek/nerdy.nvim"; flake = false; };
-    telescope-emoji-nvim = { url = "github:xiyaowong/telescope-emoji.nvim"; flake = false; };
-    easypick-nvim = { url = "github:axkirillov/easypick.nvim"; flake = false; };
-    telescope-git-conflicts-nvim = { url = "github:Snikimonkd/telescope-git-conflicts.nvim"; flake = false; };
-
-    # Startup
-    alpha-nvim = { url = "github:goolord/alpha-nvim"; flake = false; };
-    fortune-nvim = { url = "github:rubiin/fortune.nvim"; flake = false; };
-
-    # Tmux Integration
-    vim-tmux-navigator-nvim = { url = "github:christoomey/vim-tmux-navigator"; flake = false; };
-    # nvim-tmux-navigator-nvim = { url = "github:alexghergh/nvim-tmux-navigation"; flake = false; };
-
-    # Stuff not in nixpkgs
+    # # Stuff not in nixpkgs
     journal-nvim = { url = "github:jakobkhansen/journal.nvim"; flake = false; };
     tiny-code-action-nvim = { url = "github:rachartier/tiny-code-action.nvim"; flake = false; };
     tiny-devicons-auto-colors-nvim = { url = "github:rachartier/tiny-devicons-auto-colors.nvim"; flake = false; };
     markdown-toc-nvim = { url = "github:ChuufMaster/markdown-toc"; flake = false; };
+    telescope-git-conflicts-nvim = { url = "github:Snikimonkd/telescope-git-conflicts.nvim"; flake = false; };
+    nerdy-nvim = { url = "github:2KAbhishek/nerdy.nvim"; flake = false; };
+    telescope-emoji-nvim = { url = "github:xiyaowong/telescope-emoji.nvim"; flake = false; };
+    easypick-nvim = { url = "github:axkirillov/easypick.nvim"; flake = false; };
 
-    # UI and Aesthetics
-    rose-pine-nvim = { url = "github:rose-pine/neovim"; flake = false; };
-    nvim-lualine-nvim = { url = "github:nvim-lualine/lualine.nvim"; flake = false; };
-    helpview-nvim = { url = "github:OXY2DEV/helpview.nvim"; flake = false; };
-    markview-nvim = { url = "github:OXY2DEV/markview.nvim"; flake = false; };
-    nvim-web-devicons-nvim = { url = "github:nvim-tree/nvim-web-devicons"; flake = false; };
-    dressing-nvim = { url = "github:stevearc/dressing.nvim"; flake = false; };
-
-    # Writing
-    mkdnflow-nvim = { url = "github:jakewvincent/mkdnflow.nvim"; flake = false; };
-    quarto-nvim = { url = "github:quarto-dev/quarto-nvim"; flake = false; };
-    otter-nvim = { url = "github:jmbuhr/otter.nvim"; flake = false; };
-
-    # Image Handling
-    img-clip-nvim = { url = "github:HakonHarnes/img-clip.nvim"; flake = false; };
-    image-nvim = { url = "github:3rd/image.nvim"; flake = false; };
-
-    # Terminal Integration
-    toggleterm-nvim = { url = "github:akinsho/toggleterm.nvim"; flake = false; };
-
-    # Git Integration
-    neogit-nvim = { url = "github:NeogitOrg/neogit"; flake = false; };
-
-    # Marks
-    marks-nvim = { url = "github:chentoast/marks.nvim"; flake = false; };
-
-    # Keybinds
-    which-key-nvim = { url = "github:folke/which-key.nvim"; flake = false; };
-
-    # Nix Direnv Integration
-    nix-develop-nvim = { url = "github:figsoda/nix-develop.nvim"; flake = false; };
-
-    # Colors
-    colorize-lua-nvim = { url = "github:norcalli/nvim-colorizer.lua"; flake = false; };
-
-    # Other
-    cellular-automaton-nvim = { url = "github:Eandrju/cellular-automaton.nvim"; flake = false; };
-
-    # Vim Habits
-    # hardtime-nvim
-    # precognition-nvim
-    # }}}
+    # # }}}
   };
 }
---
+++ plugins.nix
@@ -0,0 +1,123 @@
+{ pkgs, ... }:
+
+# Plugins from nixpkgs-unstable
+
+{
+  inherit (pkgs.vimPlugins)
+
+    # Utilities
+    plenary-nvim
+
+    # Colorscheme
+    rose-pine
+
+    # LSP
+    lsp-zero-nvim
+    nvim-lspconfig
+    lspkind-nvim
+    fidget-nvim
+    barbecue-nvim
+    nvim-navic
+
+    # Snippets
+    luasnip
+
+    # Completion
+    nvim-cmp
+    cmp-nvim-lsp
+    # cmp-nvim-lua
+    cmp-buffer
+    cmp_luasnip
+    cmp-async-path
+
+
+    # DAP
+    nvim-dap
+    # nvim-dap-virtualtext
+    nvim-dap-ui
+    nvim-nio
+    # one-small-step-for-vimkind-nvim = { url = "github:jbyuki/one-small-step-for-vimkind"; flake = false; };
+    telescope-dap-nvim
+
+    # Linting
+    nvim-lint
+    tiny-inline-diagnostic-nvim
+
+    # Formatting
+    conform-nvim
+
+    # Telescope
+    telescope-nvim
+    # telescope-manix
+    telescope-project-nvim#TODO: replace
+
+    # Editing Support
+    indent-blankline-nvim
+    nvim-autopairs
+    nvim-treesitter-endwise
+    rainbow-delimiters-nvim
+    nvim-surround
+
+    # Comments
+    comment-nvim
+    todo-comments-nvim
+
+    # File Explorers
+    triptych-nvim
+    oil-nvim
+
+    # Status Line
+    lualine-nvim
+    # lualine-lsp-progress
+    # git-prompt-string-lualine-nvim
+
+    # UI and Aesthetics
+    helpview-nvim
+    markview-nvim
+    nvim-web-devicons
+    dressing-nvim
+
+    # Image Handling
+    img-clip-nvim
+    image-nvim
+
+    # Git
+    neogit
+
+    # Marks
+    marks-nvim
+
+    # Terminal Integration
+    toggleterm-nvim
+
+    # Keybinds
+    which-key-nvim
+
+    # Nix Direnv
+    nix-develop-nvim
+
+    # Colors
+    nvim-colorizer-lua
+
+
+    # Writing
+    obsidian-nvim
+    mkdnflow-nvim#TODO: remove
+    quarto-nvim
+    otter-nvim
+
+    # Startup
+    fortune-nvim
+    alpha-nvim
+
+    # Tmux Integration
+    vim-tmux-navigator
+    # nvim-tmux-navigator-nvim = { url = "github:alexghergh/nvim-tmux-navigation"; flake = false; };
+
+    # Other
+    cellular-automaton-nvim
+    # hardtime-nvim
+    # precognition-nvim
+    ;
+}
+
