99d384ecc4e528ce19da4f88d333de09077622f9
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-11-26 22:02:41 -0500

    remove unused highlights from lsp-zero.lua

--- nebvim/lua/neb/plugins/lsp/lsp-zero.lua
+++ nebvim/lua/neb/plugins/lsp/lsp-zero.lua
@@ -1,30 +1,14 @@
 require("lsp-zero")
 
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 lsp_zero.set_sign_icons({
-  error = '✘',
-  warn = '▲',
-  hint = '⚑',
-  info = '»'
+	error = "✘",
+	warn = "▲",
+	hint = "⚑",
+	info = "»",
 })
-
--- Custom colors for diagnostic signs
--- vim.cmd([[
---   highlight DiagnosticSignError guifg=#ff0000 guibg=NONE
---   highlight DiagnosticSignWarn guifg=#ffff00 guibg=NONE
---   highlight DiagnosticSignHint guifg=#00ffff guibg=NONE
---   highlight DiagnosticSignInfo guifg=#0000ff guibg=NONE
--- ]])
-
--- Optional: Change colors for the diagnostic text as well
--- vim.cmd([[
---   highlight DiagnosticError guifg=#ff0000
---   highlight DiagnosticWarn guifg=#ffff00
---   highlight DiagnosticHint guifg=#00ffff
---   highlight DiagnosticInfo guifg=#0000ff
--- ]])
