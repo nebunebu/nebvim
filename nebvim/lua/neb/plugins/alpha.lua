cb4ce382134a76059892cddfc3d5d6ff3cac53b0
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-11-26 23:52:33 -0500

    update alpha and snacks

--- nebvim/lua/neb/plugins/alpha.lua
+++ nebvim/lua/neb/plugins/alpha.lua
@@ -32,7 +32,7 @@ local links = {
 		{ type = "padding", val = 1 },
 		dashboard.button("c", "󱄅  .nix-config", ":e $HOME/.nix-config/flake.nix <CR>"),
 		dashboard.button("n", "  nebvim", ":e $HOME/.nebvim/flake.nix <CR>"),
-		dashboard.button("a", " ags", ":e $HOME/.config/ags/flake.nix <CR>"),
+		dashboard.button("a", "  ags", ":e $HOME/.config/ags/flake.nix <CR>"),
 		dashboard.button("v", "󱉼  obsidian vault", ":e $HOME/.vault/index.md <CR>"),
 		-- dashboard.button("b", " blog", ":qa<CR>"),
 		-- dashboard.button("w", "󰖬  wiki", ":e $HOME/.wiki/index.md <CR>"),
@@ -82,24 +82,25 @@ require("alpha").setup(theta.config)
 
 vim.api.nvim_create_user_command("AlphaTab", "tabnew | Alpha", {})
 
-vim.api.nvim_create_autocmd("User", {
-	pattern = "AlphaReady",
-	desc = "hide cursor for alpha",
-	callback = function()
-		local hl = vim.api.nvim_get_hl(0, { name = "Cursor" })
-		hl.blend = 100
-		vim.api.nvim_set_hl(0, "Cursor", hl)
-		vim.opt.guicursor:append("a:Cursor/lCursor")
-	end,
-})
-
-vim.api.nvim_create_autocmd("BufUnload", {
-	buffer = 0,
-	desc = "show cursor after alpha",
-	callback = function()
-		local hl = vim.api.nvim_get_hl(0, { name = "Cursor" })
-		hl.blend = 0
-		vim.api.nvim_set_hl(0, "Cursor", hl)
-		vim.opt.guicursor:remove("a:Cursor/lCursor")
-	end,
-})
+-- FIX: hiding cursor
+-- vim.api.nvim_create_autocmd("User", {
+-- 	pattern = "AlphaReady",
+-- 	desc = "hide cursor for alpha",
+-- 	callback = function()
+-- 		local hl = vim.api.nvim_get_hl(0, { name = "Cursor" })
+-- 		hl.blend = 100
+-- 		vim.api.nvim_set_hl(0, "Cursor", hl)
+-- 		vim.opt.guicursor:append("a:Cursor/lCursor")
+-- 	end,
+-- })
+--
+-- vim.api.nvim_create_autocmd("BufUnload", {
+-- 	buffer = 0,
+-- 	desc = "show cursor after alpha",
+-- 	callback = function()
+-- 		local hl = vim.api.nvim_get_hl(0, { name = "Cursor" })
+-- 		hl.blend = 0
+-- 		vim.api.nvim_set_hl(0, "Cursor", hl)
+-- 		vim.opt.guicursor:remove("a:Cursor/lCursor")
+-- 	end,
+-- })
--- nebvim/lua/neb/plugins/snacks/init.lua
+++ nebvim/lua/neb/plugins/snacks/init.lua
@@ -3,24 +3,19 @@ local M = {}
 function M.setup()
 	local config = {
 		bigfile = require("neb.plugins.snacks.bigfile"),
-		notifier = require("neb.plugins.snacks.notifier"),
-		quickfile = require("neb.plugins.snacks.quickfile"),
-		lazygit = require("neb.plugins.snacks.lazygit"),
-		--  NOTE: keep using alpha
+		-- bufdelete = require("neb.plugins.snacks.bufdelete"),
 		-- dashboard = require("neb.plugins.snacks.dashboard"),
-
-		-- require("neb.plugins.snacks.bufdelete")
-		-- require("neb.plugins.snacks.debug")
-		-- require("neb.plugins.snacks.git")
-		-- require("neb.plugins.snacks.gitbrowse")
-		-- require("neb.plugins.snacks.notify")
-		-- require("neb.plugins.snacks.notifier")
-		-- require("neb.plugins.snacks.quickfile")
-		-- require("neb.plugins.snacks.rename")
-		-- require("neb.plugins.snacks.statuscolumn")
-		-- require("neb.plugins.snacks.toggle")
-		-- require("neb.plugins.snacks.win")
-		-- require("neb.plugins.snacks.words")
+		debug = require("neb.plugins.snacks.debug"),
+		git = require("neb.plugins.snacks.git"),
+		-- gitbrowse = require("neb.plugins.snacks.gitbrowse"),
+		lazygit = require("neb.plugins.snacks.lazygit"),
+		notifier = require("neb.plugins.snacks.notifier"),
+		notify = require("neb.plugins.snacks.notify"),
+		quickfile = require("neb.plugins.snacks.quickfile"),
+		statuscolumn = require("neb.plugins.snacks.statuscolumn"),
+		-- toggle = require("neb.plugins.snacks.toggle"),
+		win = require("neb.plugins.snacks.win"),
+		words = require("neb.plugins.snacks.words"),
 	}
 
 	require("snacks").setup(config)
