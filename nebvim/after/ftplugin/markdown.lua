ffd5e2d05ded55d37242f27aedbe92517d72174c
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-14 00:13:22 -0500

    add colorcolumn for md

--- nebvim/after/ftplugin/markdown.lua
+++ nebvim/after/ftplugin/markdown.lua
@@ -2,9 +2,10 @@ vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.smarttab = true
+vim.opt_local.colorcolumn = "60"
 
-vim.opt_local.textwidth = 80  -- Wrap at 80 characters
-vim.opt_local.spell = true    -- Enable spell checking for Markdown files
+vim.opt_local.textwidth = 80 -- Wrap at 80 characters
+vim.opt_local.spell = true -- Enable spell checking for Markdown files
 
 -- Prevent these settings from being overridden
 vim.b.did_ftplugin = 1
