0b9beec57655efe01e1f754d62dbdc5c12893d72
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-19 22:34:14 -0500

    put new notes in capture

--- nebvim/lua/neb/plugins/obsidian.lua
+++ nebvim/lua/neb/plugins/obsidian.lua
@@ -36,8 +36,6 @@ return {
 	--
 	-- mode = { "x" },
 	-- { "<leader>onl", "<cmd>ObsidianLinkNew<CR>", desc = "Create a new link from visually selected text" },
-	-- { "<", "<gv", desc = "Dedent and keep selection" },
-	-- { ">", ">gv", desc = "Indent and keep selection" },
 
 	after = function()
 		require("obsidian").setup({
@@ -45,13 +43,19 @@ return {
 				{
 					name = ".vault",
 					path = "~/.vault",
+					overrides = {
+						notes_subdir = [[32 - Capture]],
+					},
 				},
 			},
 			completion = {
 				nvim_cmp = false,
 				min_chars = 2,
 			},
-			new_notes_location = "current_dir",
+
+			new_notes_location = "notes_subdir",
+			open_notes_in = "vsplit",
+
 			preferred_link_style = "markdown",
 			disable_frontmatter = false,
 			-- open_app_foreground = true,
@@ -60,50 +64,9 @@ return {
 			},
 			sort_by = "modified",
 			sort_reversed = true,
-
-			open_notes_in = "current",
-
+			-- open_notes_in = [[32 - Capture]],
 			ui = {
 				enable = false, -- set to false to disable all additional syntax features
-				update_debounce = 200, -- update delay after a text change (in milliseconds)
-				max_file_length = 5000, -- disable UI features for files with more than this many lines
-				-- Define how various check-boxes are displayed
-				checkboxes = {
-					-- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-					[" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-					["x"] = { char = "", hl_group = "ObsidianDone" },
-					[">"] = { char = "", hl_group = "ObsidianRightArrow" },
-					["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
-					["!"] = { char = "", hl_group = "ObsidianImportant" },
-					-- Replace the above with this if you don't have a patched font:
-					-- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
-					-- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
-
-					-- You can also add more custom ones...
-				},
-				-- Use bullet marks for non-checkbox lists.
-				bullets = { char = "•", hl_group = "ObsidianBullet" },
-				external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-				-- Replace the above with this if you don't have a patched font:
-				-- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-				reference_text = { hl_group = "ObsidianRefText" },
-				highlight_text = { hl_group = "ObsidianHighlightText" },
-				tags = { hl_group = "ObsidianTag" },
-				block_ids = { hl_group = "ObsidianBlockID" },
-				hl_groups = {
-					-- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
-					ObsidianTodo = { bold = true, fg = "#f78c6c" },
-					ObsidianDone = { bold = true, fg = "#89ddff" },
-					ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
-					ObsidianTilde = { bold = true, fg = "#ff5370" },
-					ObsidianImportant = { bold = true, fg = "#d73128" },
-					ObsidianBullet = { bold = true, fg = "#89ddff" },
-					ObsidianRefText = { underline = true, fg = "#c792ea" },
-					ObsidianExtLinkIcon = { fg = "#c792ea" },
-					ObsidianTag = { italic = true, fg = "#89ddff" },
-					ObsidianBlockID = { italic = true, fg = "#89ddff" },
-					ObsidianHighlightText = { bg = "#75662e" },
-				},
 			},
 		})
 	end,
