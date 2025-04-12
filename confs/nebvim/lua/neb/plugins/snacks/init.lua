7a92d2492187618fd1385152a699b999fa62767e
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-04-12 15:53:48 -0400

    deactivate snacks image and dashboard

--- confs/nebvim/lua/neb/plugins/snacks/init.lua
+++ confs/nebvim/lua/neb/plugins/snacks/init.lua
@@ -4,8 +4,8 @@ function M.setup()
 	local config = {
 		bigfile = require("neb.plugins.snacks.bigfile"),
 		-- bufdelete = require("neb.plugins.snacks.bufdelete"),
-		dashboard = require("neb.plugins.snacks.dashboard"),
-		image = require("neb.plugins.snacks.image"),
+		-- dashboard = require("neb.plugins.snacks.dashboard"),
+		-- image = require("neb.plugins.snacks.image"),
 		debug = require("neb.plugins.snacks.debug"),
 		git = require("neb.plugins.snacks.git"),
 		-- gitbrowse = require("neb.plugins.snacks.gitbrowse"),
