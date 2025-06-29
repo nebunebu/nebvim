975cb717bef4d3985ee2db721ddc2afc1b4745b4
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-06-29 04:41:06 -0400

    fix gemini api

--- confs/nebvim/lua/neb/plugins/avante.lua
+++ confs/nebvim/lua/neb/plugins/avante.lua
@@ -1,11 +1,8 @@
-return {
-  "yetone/avante.nvim",
-  opts = {
-    provider = "gemini",
-    providers = {
-      gemini = {
-        model = "gemini-2.5-pro",
-      },
-    },
-  },
-}
+require("avante").setup({
+	provider = "gemini",
+	providers = {
+		gemini = {
+			model = "gemini-2.5-pro",
+		},
+	},
+})
