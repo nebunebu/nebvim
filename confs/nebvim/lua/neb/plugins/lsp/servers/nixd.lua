5e84d7b0218b8aa04dc46d5d3edfff020c12bfc5
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-09-19 20:34:17 -0400

    remove garbage from nixd

--- confs/nebvim/lua/neb/plugins/lsp/servers/nixd.lua
+++ confs/nebvim/lua/neb/plugins/lsp/servers/nixd.lua
@@ -5,17 +5,17 @@ vim.lsp.config["nixd"] = {
 	settings = {
 		nixd = {
 			nixpkgs = {
-				expr = 'let flakePath = builtins.toString ./.; in if builtins.pathExists (flakePath + "/flake.nix") then import (builtins.getFlake ("git+file://" + flakePath)).inputs.nixpkgs { } else import (builtins.getFlake ("git+file://" + builtins.getEnv "HOME" + "/.nix-config")).inputs.nixpkgs { }',
+				-- expr = 'let flakePath = builtins.toString ./.; in if builtins.pathExists (flakePath + "/flake.nix") then import (builtins.getFlake ("git+file://" + flakePath)).inputs.nixpkgs { } else import (builtins.getFlake ("git+file://" + builtins.getEnv "HOME" + "/.nix-config")).inputs.nixpkgs { }',
 			},
 			formatting = {
 				command = { "nixfmt" },
 			},
 			options = {
 				nixos = {
-					expr = '(builtins.getFlake ("git+file://" + builtins.getEnv "HOME" + "/.nix-config")).nixosConfigurations.${builtins.getEnv "HOSTNAME"}.options',
+					-- expr = '(builtins.getFlake ("git+file://" + builtins.getEnv "HOME" + "/.nix-config")).nixosConfigurations.${builtins.getEnv "HOSTNAME"}.options',
 				},
 				home_manager = {
-					expr = '(builtins.getFlake ("git+file://" + builtins.getEnv "HOME" + "/.nix-config")).nixosConfigurations.${builtins.getEnv "HOSTNAME"}.options.home-manager.users.type.getSubOptions []',
+					-- expr = '(builtins.getFlake ("git+file://" + builtins.getEnv "HOME" + "/.nix-config")).nixosConfigurations.${builtins.getEnv "HOSTNAME"}.options.home-manager.users.type.getSubOptions []',
 				},
 			},
 			-- diagnostic = {},
