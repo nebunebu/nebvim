8e7d8c31fbf4a4ec3433c67da71ebde342175e89
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-09-28 17:27:43 -0400

    update baleia

--- confs/manvim/lua/man/plugins/baleia.lua
+++ confs/manvim/lua/man/plugins/baleia.lua
@@ -1,29 +1,45 @@
--- lua/man/plugins/baleia.lua (or wherever you configure it)
+-- lua/man/plugins/baleia.lua
 local baleia = require("baleia").setup({
 	strip_ansi_codes = true,
 	log = "DEBUG",
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "man",
-	callback = function()
-		-- don't set nomodifiable here
-		vim.keymap.set("n", "q", ":q<CR>", { buffer = true })
-		vim.keymap.set("n", "ZZ", ":q<CR>", { buffer = true })
-		vim.keymap.set("n", "ZQ", ":q<CR>", { buffer = true })
-	end,
-})
+local grp = vim.api.nvim_create_augroup("ManBaleia", { clear = true })
 
-vim.api.nvim_create_autocmd("BufWinEnter", {
+-- If the pager fed us via stdin, ft may be empty; prefer $MAN_PN signal.
+local function is_man_buf(buf)
+	if vim.bo[buf].filetype == "man" then
+		return true
+	end
+	if os.getenv("MAN_PN") then
+		vim.bo[buf].filetype = "man"
+		return true
+	end
+	return false
+end
+
+vim.api.nvim_create_autocmd({ "StdinReadPost", "BufReadPost" }, {
+	group = grp,
 	pattern = "*",
-	callback = function()
-		if vim.bo.filetype == "man" then
-			-- temporarily allow edits so Baleia can strip SGR sequences
-			local was_modifiable = vim.bo.modifiable
-			vim.bo.modifiable = true
-			baleia.once(0)
-			vim.bo.modifiable = was_modifiable or false
-			vim.bo.modified = false
+	callback = function(ev)
+		local buf = ev.buf
+		if vim.b[buf].baleia_applied then
+			return
 		end
+		if not is_man_buf(buf) then
+			return
+		end
+
+		local was_mod = vim.bo[buf].modifiable
+		vim.bo[buf].modifiable = true
+		baleia.once(buf)
+		vim.bo[buf].modifiable = was_mod or false
+		vim.bo[buf].modified = false
+		vim.b[buf].baleia_applied = true
+
+		-- sane quit keys for pager use
+		vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = buf, silent = true })
+		vim.keymap.set("n", "ZZ", "<cmd>q<cr>", { buffer = buf, silent = true })
+		vim.keymap.set("n", "ZQ", "<cmd>q!<cr>", { buffer = buf, silent = true })
 	end,
 })
