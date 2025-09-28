dd052287b6df854cfa491f45f73dc146646cbaf5
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-09-27 22:28:04 -0400

    add baleia

--- confs/manvim/lua/man/plugins/baleia.lua
+++ confs/manvim/lua/man/plugins/baleia.lua
@@ -1,10 +1,10 @@
-local baleia = require("baleia")
-
-baleia.setup({})
+vim.g.baleia_instance = require("baleia").setup({})
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "man",
 	callback = function()
-		baleia.once(0)
+		if vim.g.baleia_instance then
+			vim.g.baleia_instance.once(0) -- 0 is shorthand for the current buffer
+		end
 	end,
 })
