bb4aed4cdd7717543bd08574d611cac57cde2b8b
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-10-24 14:11:29 -0400

    add options to manvim

---
+++ confs/manvim/lua/man/options.lua
@@ -0,0 +1,42 @@
+local function set_options(scope, options)
+	for option, value in pairs(options) do
+		vim[scope][option] = value
+	end
+end
+
+set_options("g", {
+	mapleader = " ",
+})
+
+set_options("opt", {
+	clipboard = "unnamedplus",
+	autochdir = true,
+	-- numbers
+	number = true,
+	relativenumber = true,
+	-- tabs
+	expandtab = true,
+	shiftwidth = 2,
+	tabstop = 2,
+	smarttab = true,
+	-- search
+	ignorecase = true,
+	smartcase = true,
+	incsearch = true,
+	-- linebreaks
+	wrap = true,
+	linebreak = true,
+	showbreak = "↪ ",
+	breakindent = true,
+	-- swap and undo files
+	undofile = true,
+	swapfile = false,
+	-- folds
+	foldmethod = "marker",
+	foldmarker = "{{{,}}}",
+	-- splits
+	splitright = true,
+
+	-- other
+	fillchars = { eob = " " },
+})
