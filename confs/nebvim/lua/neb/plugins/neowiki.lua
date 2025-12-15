9f01cb86dff720604d3ede9632cee5b266f7cf8f
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-12-15 16:05:29 -0500

    add quartz to neowiki

--- confs/nebvim/lua/neb/plugins/neowiki.lua
+++ confs/nebvim/lua/neb/plugins/neowiki.lua
@@ -10,6 +10,7 @@ return {
 		require("neowiki").setup({
 			wiki_dirs = {
 				{ name = "Wiki", path = "~/.wiki" },
+				{ name = "quartz", path = "~/Projects/quartz/content" },
 				{ name = "Scratchpad", path = "~/.wiki/scratchpad" },
 			},
 			index_file = "index.md",
