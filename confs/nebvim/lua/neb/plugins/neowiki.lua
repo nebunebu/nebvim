42d2740f645e4ff88922ffaf80119b89d35a0c2e
nebunebu <nebu.nebuchadnezzar@gmail.com>
2026-01-10 13:30:59 -0500

    fix neowiki navigation

--- confs/nebvim/lua/neb/plugins/neowiki.lua
+++ confs/nebvim/lua/neb/plugins/neowiki.lua
@@ -4,52 +4,32 @@ return {
 	ft = "markdown",
 	keys = {
 		{ "<leader>ww", "<cmd>lua require('neowiki').open_wiki()<cr>", desc = "Open Wiki" },
-		{ "<leader>wW", "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = "Open Floating Wiki" },
 		{ "<leader>wT", "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>", desc = "Open Wiki in Tab" },
 	},
 	after = function()
 		require("neowiki").setup({
 			wiki_dirs = {
-				{ name = "Wiki", path = "~/.wiki" },
 				{ name = "quartz", path = "~/Projects/quartz/content" },
-				{ name = "Scratchpad", path = "~/.wiki/scratchpad" },
+				{ name = "scratchpad", path = "~/.scratchpad" },
 			},
 			index_file = "index.md",
 			discover_nested_roots = true,
 
 			keymaps = {
-				-- In Normal mode, follows the link under the cursor.
-				-- In Visual mode, creates a link from the selection.
 				action_link = "<CR>",
 				action_link_vsplit = "<S-CR>",
 				action_link_split = "<C-CR>",
-				-- Toggles the status of a gtd item.
-				-- Works on the current line in Normal mode and on the selection in Visual mode.
 				toggle_task = "<leader>wt",
-				-- Jumps to the next link in the buffer.
 				next_link = "<Tab>",
-				-- Jumps to the previous link in the buffer.
 				prev_link = "<S-Tab>",
-				-- Jumps to the index page of the current wiki.
 				jump_to_index = "<Backspace>",
-				-- Deletes the current wiki page.
 				delete_page = "<leader>wd",
-				-- Removes all links in the current file that point to non-existent pages.
 				cleanup_links = "<leader>wc",
+				navigate_back = "[[",
+				navigate_forward = "]]",
 			},
 
-			-- Configuration for the GTD functionality.
-			-- gtd = {
-			-- 	-- Set to false to disable the progress percentage virtual text.
-			-- 	show_gtd_progress = true,
-			-- 	-- The highlight group to use for the progress virtual text.
-			-- 	gtd_progress_hl_group = "Comment",
-			-- },
-
-			-- Configuration for opening wiki in floating window.
 			floating_wiki = {
-				-- Config for nvim_open_win(). Defines the window's structure,
-				-- position, and border.
 				open = {
 					relative = "editor",
 					width = 0.85,
